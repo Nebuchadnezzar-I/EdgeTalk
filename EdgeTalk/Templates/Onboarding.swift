23d305f896f7ad025fe4b43b9d4e982f7f2fd161
Michal Ukropec <ukropec.michal.jr@gmail.com>
2025-03-10 21:57:11 +0100

    Added stuff

--- EdgeTalk/Templates/Onboarding.swift
+++ EdgeTalk/Templates/Onboarding.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SnapKit
+import Then
+import UIKit
+
+func GenerateOnboardingText(
+    mainText: String, mainTextLoC: Int,
+    subText: String, subTextLoC: Int
+) -> UIView {
+    let container = UIView();
+
+    let bigText = UILabel().then {
+        $0.text = mainText
+        $0.numberOfLines = mainTextLoC
+        $0.font = .systemFont(ofSize: 40, weight: .bold)
+        $0.textColor = .white
+    }
+
+    let smallText = UILabel().then {
+        $0.text = subText
+        $0.numberOfLines = subTextLoC
+        $0.font = .systemFont(ofSize: 16, weight: .regular)
+        $0.textColor = .white
+    }
+    
+    container.addSubview(bigText)
+    container.addSubview(smallText)
+    
+    bigText.snp.makeConstraints { make in
+        make.top.equalToSuperview()
+        make.left.equalToSuperview()
+        make.right.equalToSuperview()
+    }
+    
+    smallText.snp.makeConstraints { make in
+        make.top.equalTo(bigText.snp.bottom).offset(20)
+        make.left.equalToSuperview()
+        make.right.equalToSuperview()
+        make.bottom.equalToSuperview()
+    }
+    
+    return container
+}
--- EdgeTalk/ViewController.swift
+++ EdgeTalk/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let parentView = UIView().then {
+            $0.backgroundColor = .black
+        }
+        
+        let Onboarding1 = GenerateOnboardingText(
+            mainText: "Negotiate smart\nWin more deals", mainTextLoC: 2,
+            subText: "Master the art of persuasion\nwith AI-driven insights and\nreal-world negotiation tactics.", subTextLoC: 3
+        );
+        
+        parentView.addSubview(Onboarding1)
+        view.addSubview(parentView)
+        
+        let safe = view.safeAreaLayoutGuide.snp
+        parentView.snp.makeConstraints { make in
+            make.top.equalTo(safe.top).offset(16)
+            make.left.equalTo(safe.left).offset(16)
+            make.bottom.equalTo(safe.bottom).offset(-16)
+            make.right.equalTo(safe.right).offset(-16)
+        }
+        
+        Onboarding1.snp.makeConstraints { make in
+            make.bottom.equalTo(parentView.snp.bottom).offset(-48)
+            make.left.equalTo(parentView.snp.left)
+        }
     }
-
-
 }
-
