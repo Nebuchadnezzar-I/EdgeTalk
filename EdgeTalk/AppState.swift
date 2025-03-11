d3767e027c738fca2ead3b6b1446488e82b57317
Michal Ukropec <ukropec.michal.jr@gmail.com>
2025-03-11 08:28:32 +0100

    Made Bubbles & Head text

---
+++ EdgeTalk/AppState.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalState.swift
+//  EdgeTalk
+//
+//  Created by Michal Ukropec on 11/03/2025.
+//
+
+import Foundation
---
+++ EdgeTalk/Components/BubbleRadar.swift
@@ -0,0 +1,8 @@
+//
+//  BubbleRadar.swift
+//  EdgeTalk
+//
+//  Created by Michal Ukropec on 11/03/2025.
+//
+
+import Foundation
