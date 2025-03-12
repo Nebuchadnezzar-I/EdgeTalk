1c75fae5ed732f611dcf8cc799ecdaa06a66c52b
Michal Ukropec <ukropec.michal.jr@gmail.com>
2025-03-12 18:05:18 +0100

    Changes

--- EdgeTalk/AppState.swift
+++ EdgeTalk/AppState.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import UIKit
+
+enum AppScene {
+    case OnboardingOne
+    case OnboardingTwo
+    case OnboardingThree
+
+    func createConstraints(for vc: ViewController) -> [NSLayoutConstraint] {
+        switch self {
+        case .OnboardingOne:
+            return [
+                vc.SampleLabel.centerXAnchor.constraint(
+                    equalTo: vc.view.centerXAnchor),
+                vc.SampleLabel.topAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.topAnchor, constant: 50
+                ),
+                vc.SampleLabel.leadingAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.leadingAnchor,
+                    constant: 20),
+                vc.SampleLabel.trailingAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.trailingAnchor,
+                    constant: -20),
+                vc.SampleLabel.heightAnchor.constraint(equalToConstant: 40),
+
+                vc.SampleLabelOne.centerXAnchor.constraint(
+                    equalTo: vc.view.centerXAnchor),
+                vc.SampleLabelOne.topAnchor.constraint(
+                    equalTo: vc.view.bottomAnchor, constant: 100),
+
+                vc.SampleLabelTwo.centerXAnchor.constraint(
+                    equalTo: vc.view.centerXAnchor),
+                vc.SampleLabelTwo.topAnchor.constraint(
+                    equalTo: vc.view.bottomAnchor, constant: 100),
+            ]
+
+        case .OnboardingTwo:
+            return [
+                vc.SampleLabelOne.centerXAnchor.constraint(
+                    equalTo: vc.view.centerXAnchor),
+                vc.SampleLabelOne.centerYAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.centerYAnchor),
+                vc.SampleLabelOne.leadingAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.leadingAnchor,
+                    constant: 30),
+                vc.SampleLabelOne.trailingAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.trailingAnchor,
+                    constant: -30),
+                vc.SampleLabelOne.heightAnchor.constraint(equalToConstant: 50),
+            ]
+
+        case .OnboardingThree:
+            return [
+                vc.SampleLabelTwo.leadingAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.leadingAnchor,
+                    constant: 20),
+                vc.SampleLabelTwo.trailingAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.trailingAnchor,
+                    constant: -20),
+                vc.SampleLabelTwo.bottomAnchor.constraint(
+                    equalTo: vc.view.safeAreaLayoutGuide.bottomAnchor,
+                    constant: -40),
+                vc.SampleLabelTwo.heightAnchor.constraint(equalToConstant: 60),
+            ]
+        }
+    }
+
+}
--- EdgeTalk/ViewController.swift
+++ EdgeTalk/ViewController.swift
@@ -5,32 +5,93 @@
 //  Created by Michal Ukropec on 10/03/2025.
 //
 
-import SnapKit
-import Then
 import UIKit
 
 class ViewController: UIViewController {
-    private let onboardingView = OnboardingView()
+    let SampleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Onboarding Screen One"
+        label.textAlignment = .center
+        label.backgroundColor = .systemBlue
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let SampleLabelOne: UILabel = {
+        let label = UILabel()
+        label.text = "Onboarding Screen Two"
+        label.textAlignment = .center
+        label.backgroundColor = .systemGreen
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let SampleLabelTwo: UILabel = {
+        let label = UILabel()
+        label.text = "Onboarding Screen Three"
+        label.textAlignment = .center
+        label.backgroundColor = .systemRed
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private var activeConstraints: [NSLayoutConstraint] = []
+    private var currentScene: AppScene = .OnboardingOne
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupOnboardingView()
+        view.addSubview(SampleLabel)
+        view.addSubview(SampleLabelOne)
+        view.addSubview(SampleLabelTwo)
+
+        switchToScene(.OnboardingOne)
+
+        setupNextButton()
     }
 
-    private func setupOnboardingView() {
-        view.addSubview(onboardingView)
-        onboardingView.translatesAutoresizingMaskIntoConstraints = false
+    func switchToScene(_ scene: AppScene) {
+        NSLayoutConstraint.deactivate(activeConstraints)
+
+        currentScene = scene
+        activeConstraints = scene.createConstraints(for: self)
+
+        NSLayoutConstraint.activate(activeConstraints)
+
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    func setupNextButton() {
+        let button = UIButton(type: .system)
+        button.setTitle("Next Scene", for: .normal)
+        button.addTarget(
+            self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(button)
 
         NSLayoutConstraint.activate([
-            onboardingView.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor),
-            onboardingView.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor),
-            onboardingView.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor),
-            onboardingView.bottomAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            button.widthAnchor.constraint(equalToConstant: 150),
+            button.heightAnchor.constraint(equalToConstant: 44),
         ])
+    }
+
+    @objc func nextButtonTapped() {
+        switch currentScene {
+        case .OnboardingOne:
+            switchToScene(.OnboardingTwo)
+        case .OnboardingTwo:
+            switchToScene(.OnboardingThree)
+        case .OnboardingThree:
+            switchToScene(.OnboardingOne)
+        }
     }
 }
